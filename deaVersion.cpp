b664c18691b2925bf1956fee6d8984c4b1fb3ac6
Zack queen <mhmmadd.mufid@gmail.com>
2025-10-25 23:55:08 +0700

    Training

---
+++ deaVersion.cpp
@@ -0,0 +1,25 @@
+// Dalam File ini yang tereksekusi pertama kali adalam "MAIN"
+#include <iostream>
+
+// int xxx () {
+//     std::cout << 'Hallo Rosaa';     // String Type Data
+//     return 0;
+// }
+
+int xxx () {
+    std::cout << false;
+    return 0;
+}
+
+int main () {
+    xxx();
+}
+
+
+
+
+
+
+
+
+// Tugas pertama dari bang dea afrizal = membuat suatu output dengan hasil berikut | hello, yeah "2 enter", nama kalian siapa 
---
+++ main.cpp
@@ -0,0 +1,19 @@
+// INFO LEBIH LANJUT = C++ Referencies
+
+// IOstream =  I -> Input   
+// IOstream =  O -> Output  
+#include <iostream>
+using namespace std;            // std = Library bawaan dari C++
+                                // cout = c out
+int main () {                   // Dipanggil dengan = :: 
+    cout << "Halo adik rosaaa" << endl << "Cuteness" 
+    << endl << endl << endl << "Muhammad Mufid Azhar";
+    return 0;
+}
+
+
+
+
+
+// Fungsi "endl" = Untuk memberi space dengan kalimat
+// jika memakai "USING NAMESPACE STD;" maka std nya sudah dinyatakan global/  keseluruhan dalam file ini

a758e39c3c884469eb4ab1caea9163eebcc08548
Mufid Axelsen <mhmmadd.mufid@gmail.com>
2025-11-16 19:35:55 +0700

    step 1,3

--- main.cpp
+++ main.cpp
@@ -26,6 +26,27 @@ int main () {                   // Dipanggil dengan = ::
     // Menampilkan hasil input
     cout << "Halo, " << nama << "!" << endl;
     cout << "Wah, umurmu " << umur << " tahun ya." << endl;
+
+    // Fitur tambahan: cek status umur
+    if (umur < 12) {
+        cout << "Kamu masih kecil, jangan lupa belajar ya!" << endl;
+    } else if (umur < 18) {
+        cout << "Kamu remaja, semangat mengejar impianmu!" << endl;
+    } else if (umur < 30) {
+        cout << "Kamu sudah dewasa muda, terus berkembang!" << endl;
+    } else {
+        cout << "Kamu sudah berpengalaman, terus memberi inspirasi!" << endl;
+    }
+
+    cout << endl;
+
+    // Fitur tambahan: menghitung tahun lahir
+    int tahunSekarang = 2025;
+    int tahunLahir = tahunSekarang - umur;
+
+    cout << "Oh iya, berarti kamu lahir sekitar tahun " << tahunLahir << "." << endl;
+
+    cout << "\nTerima kasih sudah mencoba program sederhana ini :)" << endl;
     
     return 0;
 }

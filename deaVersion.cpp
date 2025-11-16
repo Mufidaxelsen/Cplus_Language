efe4fc95a809a79207bc45816bb0885f3350f074
Mufid Axelsen <mhmmadd.mufid@gmail.com>
2025-11-17 03:55:25 +0700

    step 1,4

--- deaVersion.cpp
+++ deaVersion.cpp
@@ -1,19 +1,49 @@
 // Dalam File ini yang tereksekusi pertama kali adalam "MAIN"
 #include <iostream>
-
+using namespace std;
 // int xxx () {
 //     std::cout << 'Hallo Rosaa';     // String Type Data
 //     return 0;
 // }
 
-int xxx () {
+int zzz () {
     std::cout << false;
     return 0;
 }
 
 int main () {
-    xxx();
+    zzz();
 }
+
+
+// Fungsi xxx: mencetak nilai boolean dalam bentuk angka (0 untuk false)
+int xxx() {
+    cout << "Output dari fungsi xxx(): " << false << endl; 
+    return 0;
+}
+
+// Fungsi tambahan: mengubah boolean menjadi teks "true"/"false"
+void tampilkanBoolean(bool nilai) {
+    if (nilai) {
+        cout << "Nilai boolean ini adalah: true" << endl;
+    } else {
+        cout << "Nilai boolean ini adalah: false" << endl;
+    }
+}
+
+int main() {
+    // Memanggil fungsi xxx()
+    xxx();
+
+    cout << endl;
+
+    // Menggunakan fungsi baru
+    bool contohNilai = false;
+    tampilkanBoolean(contohNilai);
+
+    return 0;
+}
+
 
 
 

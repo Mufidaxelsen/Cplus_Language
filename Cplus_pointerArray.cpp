9d48ce9db5995fcbc7194ce8e4158ad0d4fa2773
mufidAxelsen <mhmmadd.mufid@gmail.com>
2026-02-23 23:52:49 +0700

    step 3

---
+++ Cplus_pointerArray.cpp
@@ -0,0 +1,14 @@
+// Pointer ke Array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3] = {10, 20, 30};
+    int* p = arr; // pointer ke elemen pertama array
+
+    cout << *p << endl;       // 10
+    cout << *(p + 1) << endl; // 20
+    cout << *(p + 2) << endl; // 30
+
+    return 0;
+}
